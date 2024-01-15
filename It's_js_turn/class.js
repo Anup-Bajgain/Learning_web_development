38ebfb8ba03c7b6a0d4013fc8a4a9a4a734aec12
Anup-Bajgain <118955299+Anup-Bajgain@users.noreply.github.com>
2024-01-15 22:45:21 +0545

    .

--- It's_js_turn/class.js
+++ It's_js_turn/class.js
@@ -5,23 +5,32 @@ class Product{
         this.discount = discount;
         this.itemCode = itemCode;
     }
+    get itemName() { return this.itemName + " is the product"; }
+
+
 }
 
+class Furniture extends Product{
+    constructor(name,price,discount,itemCode){
+        super(name,price,discount,itemCode);
+}
+    get itemName() { return this.itemName + " is the Furniture"; }
+}
 let pencil = new Product("pencil",20,5,'P_43');
 
-const Product1 = class Product{
-    constructor(name,price,discount,itemCode){
-        this.itemName  = name;
-        this.price = price;
-        this.discount = discount;
-        this.itemCode = itemCode;
-    }
-    get getDiscountValue(){
-        return this.discount;
-    }
-    set setDiscountValue(value){
-        this.discount = value;
-    }
-    //pencil1.setDiscountValue=8
-}
-let pencil1 = new Product1("pencil",20,5,'P_43');
+// const Product1 = class Product{
+//     constructor(name,price,discount,itemCode){
+//         this.itemName  = name;
+//         this.price = price;
+//         this.discount = discount;
+//         this.itemCode = itemCode;
+//     }
+//     get getDiscountValue(){
+//         return this.discount;
+//     }
+//     set setDiscountValue(value){
+//         this.discount = value;
+//     }
+//     //pencil1.setDiscountValue=8
+// }
+// let pencil1 = new Product1("pencil",20,5,'P_43');
