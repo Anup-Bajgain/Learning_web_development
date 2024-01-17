eabdce24c2494c3429db9577e300c59875f6dfe0
Anup-Bajgain <118955299+Anup-Bajgain@users.noreply.github.com>
2024-01-17 23:33:21 +0545

    .

---
+++ It's_js_turn/arrays.js
@@ -0,0 +1,20 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log(array[array.length - 1]);
+
+const array2 = new Array(19, 28, 32, 44, 53, 64, 75, 87,95)
+
+//push 
+// push appends the elements to the array
+
+array2.push(62)
+
+//unshift appends the elements to the array in the 0 index or first position
+
+array2.unshift(7)
+
+//pop returns and removes the last element from the array
+//shift returns and removes the first element from the array
+
+
+//join joins all the elements from the array into string
+//split does exactly opposite of join
