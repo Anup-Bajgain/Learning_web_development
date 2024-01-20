ba5228b053c74fe461a91c5b6b3ad1150d11717d
Anup-Bajgain <118955299+Anup-Bajgain@users.noreply.github.com>
2024-01-20 22:35:17 +0545

    .

--- It's_js_turn/array_ex.js
+++ It's_js_turn/array_ex.js
@@ -1,46 +1,52 @@
 const friends = [
-    { name: 'anup',
-      height: 177,
-      weight: 65,
-      eye_color: "brown",
-      gender: 'male' 
-    },
-    { name: 'toran',
+  {
+    name: 'anup',
+    height: 177,
+    weight: 65,
+    eye_color: "brown",
+    gender: 'male'
+  },
+  {
+    name: 'toran',
     height: 160,
     weight: 53,
     eye_color: "gray",
-    gender: 'male' 
-   },
- 
-    { name: 'arun',
+    gender: 'male'
+  },
+
+  {
+    name: 'arun',
     height: 152,
     weight: 48,
     eye_color: "brown",
-    gender: 'male' 
-    },
-    { name: 'abinash_g',
+    gender: 'male'
+  },
+  {
+    name: 'abinash_g',
     height: 165,
     weight: 54,
     eye_color: "light_black",
-    gender: 'male' 
-    },
-    { name: 'abinash_y',
+    gender: 'male'
+  },
+  {
+    name: 'abinash_y',
     height: 174,
     weight: 78,
     eye_color: "normal_black",
-    gender: 'male' 
-    },
-    { name: 'yogesh',
+    gender: 'male'
+  },
+  {
+    name: 'yogesh',
     height: 165,
     weight: 52,
     eye_color: "black",
-    gender: 'male' 
-    }
+    gender: 'male'
+  }
 ];
 
 array1 = friends.map(getNameAndHeight);
 
-function getNameAndHeight(bla){
-    return [bla.name,bla.height].join(",");
+function getNameAndHeight(bla) {
+  return [bla.name, bla.height].join(",");
 }
-const names = friends.map(v=> {return [v.name,v.height].join(",")})
+const names = friends.map(v => { return [v.name, v.height].join(",") })
